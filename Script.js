1e728f7fe0310ebe3acc4087bb1d7e90a1bf2e34
Nouffles <nouf@kayenfitness.com>
2025-07-12 16:20:08 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -37,7 +37,7 @@ async function sendMessage(prompt) {
   };
 
   try {
-    const res = await fetch("https://kayen-concierge.nouf.workers.dev", {
+    const res = await fetch("https://kayen-concierge.nouf.workers.dev/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
